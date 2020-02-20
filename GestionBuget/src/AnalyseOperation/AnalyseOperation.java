7d419065fc3a1702578e8e4a74f929da6ed6072b
MONNET Quentin <psyrkoz68@gmail.com>
2020-02-20 09:18:12 +0100

    Modification de PieCharts
    Modification de la PieChart pour permettre un affichage dynamique au bon endroit

--- GestionBuget/src/AnalyseOperation/AnalyseOperation.java
+++ GestionBuget/src/AnalyseOperation/AnalyseOperation.java
@@ -2,27 +2,25 @@ package AnalyseOperation;
 
 import javax.swing.JFrame;
 
-
 public class AnalyseOperation extends JFrame {
-    
+
     PieChart p;
-    public AnalyseOperation(JFrame parent)
-    {
+
+    public AnalyseOperation(JFrame parent) {
         super();
-        p = new PieChart();
-        
+        p = new PieChart("Analyse des operation");
+
         this.setTitle("Analyse des opérations");
         this.setSize(500, 500);
         this.setLocationRelativeTo(parent);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
         this.add(p);
-        
-        populatePieChart();
+
+        //populatePieChart();
     }
-    
-    public void populatePieChart()
-    {
-        
+
+    public void populatePieChart() {
+
     }
 }
