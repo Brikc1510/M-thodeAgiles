89af4d65da9ce4728db4715ea66079a4c4803812
MONNET Quentin <psyrkoz68@gmail.com>
2020-02-20 10:00:14 +0100

    Passage de la classe de connexion en singleton

--- GestionBuget/src/connection/DB_Connection.java
+++ GestionBuget/src/connection/DB_Connection.java
@@ -10,24 +10,24 @@ import java.sql.DriverManager;
 
 public class DB_Connection {
 	
-	Connection connection=null;
+	private static Connection connection=null;
+        
+        private DB_Connection()
+        {}
 
-	public static void main(String[] args) {
-		DB_Connection obj_DB_Connection=new DB_Connection();
-        System.out.println(obj_DB_Connection.get_connection());
-	}
-
-	public Connection get_connection() {
+	public static Connection get_connection() {
 		
-		Connection connection=null;
-		try {
+		if(connection == null)
+                {
+                    try {
 			Class.forName("com.mysql.jdbc.Driver");
 			connection=DriverManager.getConnection("jdbc:mysql://localhost/applicationagile","root","");
 			System.out.println("Connection OK");
-		}
-		catch (Exception e) {
-			System.out.println(e);
-		}
+                    }
+                    catch (Exception e) {
+                            System.out.println(e);
+                    }
+                }
 		return connection;
 	}
 }
